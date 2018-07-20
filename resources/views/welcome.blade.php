dd2138446d862dc52097f1810df081204a1386fb
claire <claire@yeswedev.fr>
2018-07-20 11:57:45 +0200

    Init Projet Share Your Time on Laravel

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,61 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Share Your Time</title>
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+        {!!Html::style('css/app.css')!!}
+
+    </head>
+    <body>
+        <div class="content">
+            <header class="header container-fluid">
+                <div class="row">
+                    <div class="title m-b-md col-xs-12 col-md-4">
+                        Share Your Time
+                    </div>
+                    <div class="col-xs-12 col-md-8">
+                        <h1 class="slogan">Redonnez de la valeur à l'ACTION sociale & environnementale</h1>
+                        <nav class="links d-none d-md-inline-flex justify-content-end">
+                            <ul>
+                                <li><a href="https://laravel.com/docs" class="btn">Je propose des Action</a></li>
+                                <li><a href="https://laravel.com/docs" class="btn">Je donne de mon temps</a></li>
+                            </ul>
+                        </nav>
+                    </div>
+                </div>
+            </header>
+            <section class="actions-form">
+                <h2>Quelle action bénéficiera de votre temps?</h2>
+                <p>Votre investissement est la plus belle des aides</p>
+                <!--
+                <form>
+                    <select>
+                        <option>Actions Sociale</option>
+                        <option>Actions Environnementales</option>
+                        <option>Actions Civiles</option>
+                    </select>
+                    <input type="" />
+                </form>
+                -->
+            </section>
+            <footer>
+                <nav>
+                    <ul class="d-flex justify-content-around">
+                        <li><a href="" title="">Comment ça marche</a></li>
+                        <li><a href="" title="">Qui sommes nous</a></li>
+                        <li><a href="" title="">Mentions légales</a></li>
+                        <li><a href="" title="" class="btn">Nous contacter</a></li>
+                    </ul>
+                </nav>
+                <div>
+                    <a href="" title="facebook"></a>
+                    <a href="" title="linkedin"></a>
+                </div>
+            </footer>
+        </div>
+    </body>
+</html>
