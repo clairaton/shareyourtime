70125ad320966f3af9e1c330b60044d6a13df1e3
claire <claire@yeswedev.fr>
2018-07-20 12:06:36 +0200

    sauv 20180719

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -29,18 +29,20 @@
                 </div>
             </header>
             <section class="actions-form">
-                <h2>Quelle action bénéficiera de votre temps?</h2>
-                <p>Votre investissement est la plus belle des aides</p>
-                <!--
-                <form>
-                    <select>
-                        <option>Actions Sociale</option>
-                        <option>Actions Environnementales</option>
-                        <option>Actions Civiles</option>
-                    </select>
-                    <input type="" />
-                </form>
-                -->
+                <div class="search-action">
+                    <h2>Quelle action bénéficiera de votre temps?</h2>
+                    <p>Votre investissement est la plus belle des aides</p>
+                    <!--
+                    <form>
+                        <select>
+                            <option>Actions Sociale</option>
+                            <option>Actions Environnementales</option>
+                            <option>Actions Civiles</option>
+                        </select>
+                        <input type="" />
+                    </form>
+                    -->
+                </div>
             </section>
             <footer>
                 <nav>
