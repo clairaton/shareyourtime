4cc2f40a59c1e414a086d2fa00075559fb2d231f
claire <claire@yeswedev.fr>
2018-07-24 17:20:35 +0200

    DEV|Formulaire création d'action et recherche par type

--- app/Http/Controllers/ActionsController.php
+++ app/Http/Controllers/ActionsController.php
@@ -12,7 +12,7 @@ class ActionsController extends Controller
   * @return: all available datas of the actions table on the actions view
   */
   public function index(){
-    $actions =Action::all();
+    $actions =Action::all()->sortByDesc('id');
     return view('actions.actions', compact('actions'));
   }
 
@@ -22,7 +22,7 @@ class ActionsController extends Controller
   * @return: all available of the given type datas of the actions table on the actions view
   */
   public function displayType($type){
-    $actions = Action::where('action_type',$type)->get();
+    $actions = Action::where('action_type',$type)->orderBy('updated_at', 'DESC')->get();
     return view('actions.actions', compact('actions'));
   }
 
@@ -31,8 +31,25 @@ class ActionsController extends Controller
   * @param: $id of the action
   * @return: the action choosen on the action single view
   */
-  public function show(Action $action){
+  public function show($type,Action $action){
     return view('actions.action', compact('action'));
+  }
+
+  /**
+  * Display actions list after submitting the a search form
+  * @param: id of the action to modify if modifying else nothing
+  * @return: the action validation status
+  */
+  public function list(){
+    // Search an action list using the request data
+    // Store request datas
+    $type=request('actionType');
+    // query DB
+    if($type=='all'){
+      return ActionsController::index();
+    }else{
+      return redirect('/actions/'.$type);
+    }
   }
 
   /**
@@ -44,12 +61,23 @@ class ActionsController extends Controller
   }
 
   /**
-  * store a action
-  * @param: id of the action to modify if modifying else nothing
-  * @return: the action validation status
+  * Store an action
+  * @return: redirect to the actions page
   */
-  public function create(){
-    return view('actions.create');
+  public function store(){
+    // Create a new action using the request data
+    $action =new Action;
+    $action->action_name=request('actionName');
+    $action->action_desc=request('actionDesc');
+    $action->action_type=request('actionType');
+    $action->action_begin=request('actionBegin');
+    $action->action_end=request('actionEnd');
+    $action->action_location=request('actionLocation');
+    $action->request_company_id=1;
+    // Save it into the db
+    $action->save();
+    // Redirect to actions page
+    return redirect('/actions/'.$action->action_type);
   }
 
 
--- routes/web.php
+++ routes/web.php
@@ -11,11 +11,43 @@
 |
 */
 
+/*---------------------------------------------------------------------
+                                Actions
+---------------------------------------------------------------------*/
+
+/*
+* Display Homepage
+*/
 Route::get('/', ['uses' => 'WelcomeController@index', 'as' => 'home']);
 
-Route::get('/actions', ['uses' =>'ActionsController@index']);
+/*
+* Display all available actions
+*/
+Route::get('/actions', ['uses' =>'ActionsController@index', 'as' => 'actions']);
 
-Route::get('/actions/{type}', ['uses' =>'ActionsController@displayType']);
+/*
+* Display all available actions for a defined Type
+*/
+Route::get('/actions/{type}', ['uses' =>'ActionsController@displayType', 'as' => 'catAction'])->where('type','[a-z]+');
 
-Route::get('/action/{action}', ['uses' =>'ActionsController@show']);
+/*
+* Display a choosen action
+*/
+Route::get('/actions/{type}/{action}', ['uses' =>'ActionsController@show', 'as' => 'singleAction'])->where('type','[a-z]+');
+
+/*
+* Display actions after submitting the action search form
+*/
+Route::post('/actions', ['uses' => 'ActionsController@list', 'as' => 'actionList']);
+
+/*
+* Display the action submitting form
+*/
+Route::get('/action/create',['uses'=>'ActionsController@create']);
+
+/*
+* Display all available actions
+*/
+Route::post('/action/create',['uses'=>'ActionsController@store']);
+
 
